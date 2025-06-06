0688825245e95753b0e9d28633a12ad7e1a382b8
BioFreak95 <maximiliansalomon95@gmail.com>
2025-06-06 11:57:36 +0200

    delete code folder

--- code/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility classes and functions for MD analysis pipeline.
-"""
-
-from .DistanceCalculator import DistanceCalculator
-from .ContactCalculator import ContactCalculator
-from .ArrayConverter import ArrayConverter
-
-__all__ = ['DistanceCalculator', 'ContactCalculator', "ArrayConverter"] 
